f55d8d86b616838b4e59bdd76acb6a12b22b75dd
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-02-25 21:34:10 +0000

    Fine tune visuals

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded',() => {
+    const checkMatch = document.getElementById('checkMatch');
+    const pass = document.getElementById('password');
+    const passConfirm = document.getElementById('passConfirm');
     document.getElementById('submit').addEventListener('click', () => {
-        if (document.getElementById('password').textContent === document.getElementById('passConfirm').textContent){
+        if (document.getElementById('password').value === document.getElementById('passConfirm').value){
             document.getElementById('details').submit();
         } else {
-            
+            checkMatch.textContent = "✘";
+            pass.style.backgroundColor = 'red';
+            passConfirm.style.backgroundColor = 'red';
         }
     });
 });
