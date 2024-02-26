c89588c2f052d66b30a2c0f6d4cd9ad70a927ce9
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-02-26 16:46:22 +0000

    Move password pattern matching from CSS to JS

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ document.addEventListener('DOMContentLoaded',() => {
     const checkMatch = document.getElementById('checkMatch');
     const pass = document.getElementById('password');
     const passConfirm = document.getElementById('passConfirm');
+    const pattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?([^\w\s]|[_])).{8,}$/;
+    const regex = new RegExp (pattern);
     document.getElementById('submit').addEventListener('click', () => {
-        if (pass.value === passConfirm.value && pass.validity.valid){
+        if (pass.value === passConfirm.value && regex.test(pass.value)){
             document.getElementById('details').submit();
-        } else {
-            checkMatch.textContent = "✘";
+        } else if (pass.value === passConfirm.value && !regex.test(pass.value)){
+            checkMatch.textContent = "✘ Password not valid";
+            inputColor('red');
+        } else if (pass.value !== passConfirm.value && regex.test(pass.value)){
+            checkMatch.textContent = "✘ Passwords do not match";
             inputColor('red');
         }
     });
